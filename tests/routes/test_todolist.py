9d8a48cbf68f69f60fa1d3b9261f76ab0996be69
yoohyunk <yoohyunk20@gmail.com>
2024-10-12 02:06:42 -0600

    test_todolist_service

--- tests/routes/test_todolist.py
+++ tests/routes/test_todolist.py
@@ -1,5 +1,7 @@
 import pytest
 from flask_server.app import app
+from flask_server.services.todolist_service import TodoList
+
 
 
 @pytest.fixture(name="c")
@@ -8,7 +10,7 @@ def client():
         yield c
 
 
-def test_hello_world(c):
-    response = c.get("/todolist/get-todos")
-    assert response.status_code == 200
-    assert response.json == {"todos": []}
+
+# def test_get_todo_by_id(c):
+#     response = client.get("/todos/getTodosById?id=da0d2822-568f-45b3-be29-00e76fc616e0")
+#     assert response.status_code == 200 or response.status_code == 404
