b55cd15bfab36c9f56ce086d7f1b1716c9d8b294
yoohyunk <yoohyunk20@gmail.com>
2024-10-23 20:51:51 -0600

    create user model

---
+++ flask_server/models/user_model.py
@@ -0,0 +1,20 @@
+import hashlib
+import time
+from flask_server.db import db
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, ForeignKey
+
+
+class User(db.Model):
+    __table_name__ = 'users'
+
+    id: Mapped[str] = mapped_column(String(64), primary_key=True)  
+    name: Mapped[str]
+
+    def __init__(self, id: str, name: str):
+        self.id = self.generate_user_id(name)
+        self.name = name 
+
+    def generate_user_id(self, name: str) -> str:
+        hash_input = f"{name}{time.time()}".encode('utf-8')
+        return hashlib.sha256(hash_input).hexdigest() 
